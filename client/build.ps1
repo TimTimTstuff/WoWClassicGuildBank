94a9ca82c810a22f458e3ad3b645767614f236d9
TimTimTstuff <timo.kesselring@tstuff.de>
2019-08-10 14:27:10 +0200

    extend buildscript

--- client/build.ps1
+++ client/build.ps1
@@ -1,31 +1,54 @@
+### configuration ###
+
+#build included folders
+$folder = "01_base",
+          "02_web",
+          "03_services",
+          "09_app";
+
+#build type (tsconfig file)
+$defaultBuild = ".dev";    
+#folder with style files   
+$styleFolder = "dist\style\"
+
+
+
+
+## Build Script 
 Write-Host "Start Build"
 $build = "";
 if($null -ne $args[0]){
      $build = ".$($args[0])";
 }
-$folder = "01_base",
-          "02_web",
-          "03_services",
-          "09_app";
-          
-$tsFilesFolder = "client";
-$location = Get-Location
- $tsFilesFolder;
-$styleFolder = "$($location.Path)\dist\style\"
+$location = Get-Location;
+$styleFolder = "$($location.Path)\$($styleFolder)";
 foreach ($item in $folder) {
      
-     
+      #prepare folder
+      $buildFolder = "$($location.Path)\$($item)" 
+      
+
+      $localStyleFolder = "$($buildFolder)\style"
+      
+
+     #copy tsconfig file
+     if("" -ne $build){
           Copy-Item "$($item)\tsconfig$($build).json" -Destination "$($item)\tsconfig.json"
-     $buildFolder = "$($location.Path)\${$tsFilesFolder}\$($item)" 
-     $localStyleFolder = "$($buildFolder)\style"
-     
+     }
+
+     #tsc build
      Write-Host "Build $($item)"
      tsc -p $buildFolder 
 
+     #copy style files if they exist
      if(Test-Path $localStyleFolder){
           Write-Host "Has Style Path"
           Copy-Item "$($localStyleFolder)\*" -Destination $styleFolder -Recurse 
      }
 
+     #revert tsconfig changes
+     if("" -ne $build){
+          Copy-Item "$($item)\tsconfig$($defaultBuild).json" -Destination "$($item)\tsconfig.json"
+     }
 }
 Write-Host "Finish Build"
